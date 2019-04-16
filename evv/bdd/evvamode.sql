42238bbee3d3af4b88ea4727a59a3aac37578287
yasmine selmati <yasmineselm@gmail.com>
2019-04-16 16:35:15 -0700

    base de donnees jdida

---
+++ evv/bdd/evvamode.sql
@@ -0,0 +1,373 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 16 avr. 2019 à 23:34
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `evvamode`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id_admin` int(1) NOT NULL AUTO_INCREMENT,
+  `nom_adm` varchar(30) NOT NULL,
+  `prenom_adm` varchar(30) NOT NULL,
+  `email_adm` varchar(30) NOT NULL,
+  `tel_adm` varchar(15) NOT NULL,
+  `username_adm` varchar(45) NOT NULL,
+  `password_adm` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nom_adm`, `prenom_adm`, `email_adm`, `tel_adm`, `username_adm`, `password_adm`) VALUES
+(1, 'yasmine', 'selmati', 'yasmine@hotmail.fr', '0552232320', 'yasm', '$2y$10$HbmXY0hBCRWAhbGEtUwPtOhZWkc5pIiR33O1w4t0Qtx15PBjTBHMi'),
+(2, 'oussama', 'oussama', 'oussama@oussama.com', '3823728', 'lazrag', '$2y$10$XVA/b.1mpP8ypv73jyXir.EOZhKelQ6WwU.XDoIP6cZjbKMps3uSe');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE IF NOT EXISTS `article` (
+  `id_art` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_art` varchar(30) NOT NULL,
+  `prix_art` decimal(6,4) NOT NULL,
+  `url_img_art` varchar(100) NOT NULL,
+  `descri_art` varchar(100) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `id_catg` int(11) DEFAULT NULL,
+  `id_styls` int(11) DEFAULT NULL,
+  `id_styl` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_art`),
+  KEY `id_admin` (`id_admin`),
+  KEY `id_catg` (`id_catg`),
+  KEY `id_styls` (`id_styls`),
+  KEY `id_styl` (`id_styl`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bon_commande`
+--
+
+DROP TABLE IF EXISTS `bon_commande`;
+CREATE TABLE IF NOT EXISTS `bon_commande` (
+  `n_comd` int(11) NOT NULL AUTO_INCREMENT,
+  `date_h_comd` datetime NOT NULL,
+  `id_client` int(11) DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`n_comd`),
+  KEY `id_client` (`id_client`),
+  KEY `id_admin` (`id_admin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id_catg` int(3) NOT NULL AUTO_INCREMENT,
+  `desi_catg` varchar(45) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_catg`),
+  KEY `id_admin` (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `id_client` int(5) NOT NULL AUTO_INCREMENT,
+  `nom_client` varchar(30) NOT NULL,
+  `prenom_client` varchar(30) NOT NULL,
+  `email_client` varchar(45) NOT NULL,
+  `tel_client` varchar(15) NOT NULL,
+  `password_client` varchar(255) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_client`),
+  KEY `id_admin` (`id_admin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contenir`
+--
+
+DROP TABLE IF EXISTS `contenir`;
+CREATE TABLE IF NOT EXISTS `contenir` (
+  `n_comd` int(11) DEFAULT NULL,
+  `id_art` int(11) DEFAULT NULL,
+  `qunt` int(2) NOT NULL,
+  `prix_comd` decimal(6,2) NOT NULL,
+  KEY `n_comd` (`n_comd`),
+  KEY `id_art` (`id_art`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `date`
+--
+
+DROP TABLE IF EXISTS `date`;
+CREATE TABLE IF NOT EXISTS `date` (
+  `date_solde` date NOT NULL,
+  PRIMARY KEY (`date_solde`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evenement`
+--
+
+DROP TABLE IF EXISTS `evenement`;
+CREATE TABLE IF NOT EXISTS `evenement` (
+  `id_event` int(3) NOT NULL AUTO_INCREMENT,
+  `titre_event` varchar(45) NOT NULL,
+  `date_event` date NOT NULL,
+  `heure_event` time NOT NULL,
+  `adresse_event` varchar(45) NOT NULL,
+  `descri_event` varchar(255) NOT NULL,
+  `url_img_event` varchar(100) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `id_styls` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_event`),
+  KEY `id_admin` (`id_admin`),
+  KEY `id_styls` (`id_styls`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `point_de_vente`
+--
+
+DROP TABLE IF EXISTS `point_de_vente`;
+CREATE TABLE IF NOT EXISTS `point_de_vente` (
+  `id_pv` int(5) NOT NULL AUTO_INCREMENT,
+  `adresse_pv` varchar(45) NOT NULL,
+  `nom_res_pv` varchar(30) NOT NULL,
+  `prenom_res_pv` varchar(30) NOT NULL,
+  `email_res_pv` varchar(45) NOT NULL,
+  `tel_pv` varchar(15) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `id_styls` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_pv`),
+  KEY `id_admin` (`id_admin`),
+  KEY `id_styls` (`id_styls`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `slider`
+--
+
+DROP TABLE IF EXISTS `slider`;
+CREATE TABLE IF NOT EXISTS `slider` (
+  `id_slider` int(3) NOT NULL AUTO_INCREMENT,
+  `type_slider` varchar(45) NOT NULL,
+  `url_img_slider` varchar(255) NOT NULL,
+  `titre_slider` varchar(45) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_slider`),
+  KEY `id_admin` (`id_admin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `solder`
+--
+
+DROP TABLE IF EXISTS `solder`;
+CREATE TABLE IF NOT EXISTS `solder` (
+  `pourcentage_solde` int(3) NOT NULL,
+  `id_art` int(11) DEFAULT NULL,
+  `date_solde` date DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  KEY `id_art` (`id_art`),
+  KEY `date_solde` (`date_solde`),
+  KEY `id_admin` (`id_admin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `style`
+--
+
+DROP TABLE IF EXISTS `style`;
+CREATE TABLE IF NOT EXISTS `style` (
+  `id_styl` int(1) NOT NULL AUTO_INCREMENT,
+  `type_styl` varchar(12) NOT NULL,
+  PRIMARY KEY (`id_styl`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `style`
+--
+
+INSERT INTO `style` (`id_styl`, `type_styl`) VALUES
+(1, 'traditionnel'),
+(2, 'moderne');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `styliste`
+--
+
+DROP TABLE IF EXISTS `styliste`;
+CREATE TABLE IF NOT EXISTS `styliste` (
+  `id_styls` int(5) NOT NULL AUTO_INCREMENT,
+  `nom_styls` varchar(30) NOT NULL,
+  `prenom_styls` varchar(30) NOT NULL,
+  `tel_styls` varchar(15) NOT NULL,
+  `url_photo_styls` varchar(100) NOT NULL,
+  `url_logo_styls` varchar(100) NOT NULL,
+  `descri_styls` varchar(255) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_styls`),
+  KEY `id_admin` (`id_admin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voter`
+--
+
+DROP TABLE IF EXISTS `voter`;
+CREATE TABLE IF NOT EXISTS `voter` (
+  `id_client` int(11) DEFAULT NULL,
+  `id_art` int(11) DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `nbr_etoile` int(1) NOT NULL,
+  KEY `id_admin` (`id_admin`),
+  KEY `id_client` (`id_client`),
+  KEY `id_art` (`id_art`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `article`
+--
+ALTER TABLE `article`
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE,
+  ADD CONSTRAINT `article_ibfk_2` FOREIGN KEY (`id_catg`) REFERENCES `categories` (`id_catg`) ON DELETE CASCADE,
+  ADD CONSTRAINT `article_ibfk_3` FOREIGN KEY (`id_styls`) REFERENCES `styliste` (`id_styls`) ON DELETE CASCADE,
+  ADD CONSTRAINT `article_ibfk_4` FOREIGN KEY (`id_styl`) REFERENCES `style` (`id_styl`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `bon_commande`
+--
+ALTER TABLE `bon_commande`
+  ADD CONSTRAINT `bon_commande_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE,
+  ADD CONSTRAINT `bon_commande_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `client`
+--
+ALTER TABLE `client`
+  ADD CONSTRAINT `client_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `contenir`
+--
+ALTER TABLE `contenir`
+  ADD CONSTRAINT `contenir_ibfk_1` FOREIGN KEY (`n_comd`) REFERENCES `bon_commande` (`n_comd`) ON DELETE CASCADE,
+  ADD CONSTRAINT `contenir_ibfk_2` FOREIGN KEY (`id_art`) REFERENCES `article` (`id_art`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `evenement`
+--
+ALTER TABLE `evenement`
+  ADD CONSTRAINT `evenement_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE,
+  ADD CONSTRAINT `evenement_ibfk_2` FOREIGN KEY (`id_styls`) REFERENCES `styliste` (`id_styls`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `point_de_vente`
+--
+ALTER TABLE `point_de_vente`
+  ADD CONSTRAINT `point_de_vente_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE,
+  ADD CONSTRAINT `point_de_vente_ibfk_2` FOREIGN KEY (`id_styls`) REFERENCES `styliste` (`id_styls`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `slider`
+--
+ALTER TABLE `slider`
+  ADD CONSTRAINT `slider_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `solder`
+--
+ALTER TABLE `solder`
+  ADD CONSTRAINT `solder_ibfk_1` FOREIGN KEY (`id_art`) REFERENCES `article` (`id_art`) ON DELETE CASCADE,
+  ADD CONSTRAINT `solder_ibfk_2` FOREIGN KEY (`date_solde`) REFERENCES `date` (`date_solde`) ON DELETE CASCADE,
+  ADD CONSTRAINT `solder_ibfk_3` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `styliste`
+--
+ALTER TABLE `styliste`
+  ADD CONSTRAINT `styliste_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `voter`
+--
+ALTER TABLE `voter`
+  ADD CONSTRAINT `voter_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE,
+  ADD CONSTRAINT `voter_ibfk_2` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE,
+  ADD CONSTRAINT `voter_ibfk_3` FOREIGN KEY (`id_art`) REFERENCES `article` (`id_art`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
