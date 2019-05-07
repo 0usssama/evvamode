774a615937187782015ca0b2b9e57a90cbc84fe4
benounnas Oussama <oussamalecool@gmail.com>
2019-05-07 01:50:36 +0100

    register tests

--- admin/ajouter/ajouter_client.php
+++ admin/ajouter/ajouter_client.php
@@ -3,8 +3,8 @@
 session_start();
 
  $erreurs = [];
-$erreurs[] = 'it works';
-
+//$erreurs[] = 'it works';
+$ajouter = true;
 
  if(isset($_POST['inscrire'])){
 
@@ -21,13 +21,45 @@ $statement = $pdo->prepare($sql);
 
    //validation mazal makhssossa aprés ndirha 
    $nomClient = strip_tags(trim($_POST['nom_client']));
+    if(preg_match('/[0-9]/', $nomClient)){
+        $ajouter = false;
+        $erreurs[] = "le nom ne doit pas contenir des numéros!";
+    }
+ 
     
 
+   $prenomClient = strip_tags(trim($_POST['prenom_client']));
+
+   if(preg_match('/[0-9]/', $prenomClient)){
+    $ajouter = false;
+    $erreurs[] = "le prenom ne doit pas contenir des numéros!";
+
+}
 
 
-   $prenomClient = $_POST['prenom_client'];
-   $emailClient = $_POST['email_client'];
-   $adresseClient = $_POST['tel_client'];
+   $emailClient = strip_tags(trim($_POST['email_client']));
+   $sql = "SELECT * FROM client WHERE email_client='". $emailClient . "'";
+    if($pdo->query($sql)){
+        $result = $pdo->query($sql);
+        $array_result = $result->fetch();
+
+       if(!empty($array_result)){
+    $ajouter = false;
+    $erreurs[] = "email existe deja!";
+
+       }
+    }
+
+
+
+
+   $tel_client = strip_tags(trim($_POST['tel_client']));
+   if(!preg_match('/[0-9]/', $tel_client)){
+    $ajouter = false;
+    $erreurs[]=  $tel_client;
+    $erreurs[] = "veuillez taper un numéro valide!"; 
+
+   }
 
    $motpasseClient = password_hash($_POST['password_client'], PASSWORD_BCRYPT);
    $id_admin = 1;
@@ -39,7 +71,7 @@ $statement = $pdo->prepare($sql);
    $statement->bindValue(':nomclient', $nomClient);
    $statement->bindValue(':prenomclient', $prenomClient);
    $statement->bindValue(':emailclient', $emailClient);
-   $statement->bindValue(':telclient', $adresseClient);
+   $statement->bindValue(':telclient', $tel_client);
    $statement->bindValue(':passwordclient', $motpasseClient);
    //$statement->bindValue(':idadmin', $id_admin);
    
@@ -48,11 +80,17 @@ $statement = $pdo->prepare($sql);
 
    //}
 
-   
-  
+   if($ajouter){//tester ida Ok wela non
 
 //Execute the statement and insert our values.
 $inserted = $statement->execute();
+   }else{
+       $_SESSION['erreurs']= $erreurs;
+   header('location: ../../evv/register.php');
+       
+   }
+  
+
  
 
 //verifier si on a des résultats (true or false)
